e574aab4b9db54007869334e0bcc8530716fdd33
MayronSouza <mayron.souza@gmz.solutions>
2021-02-24 14:48:23 -0300

    implementing the UserController, inserting the getRepository for connection with the Database

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,27 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+
+import { User } from '../models/User';
 
 class UserController {
   async create(req: Request, res: Response) {
-    const body = req.body;
+    const { name, email } = req.body;
 
-    console.log(body);
+    const usersRepository = getRepository(User);
 
-    return res.send();
+    const userExists = await usersRepository.findOne({ email });
+
+    if(userExists) {
+      return res.status(400).json({ error: 'User already exists!' });
+    }
+
+    const user = usersRepository.create({ name, email });
+
+    await usersRepository.save(user);
+
+    delete user.id;
+
+    return res.status(201).json(user);
   }
 }
 
