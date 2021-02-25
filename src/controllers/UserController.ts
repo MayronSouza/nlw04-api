16c53cbb690b1f2e7f4a2b461e8649321398f626
MayronSouza <mayron.souza@gmz.solutions>
2021-02-25 07:56:18 -0300

    creating the route of the creation of surveys

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -7,9 +7,7 @@ import { UserRepository } from '../repositories/UserRepository';
 class UserController {
   async create(req: Request, res: Response) {
     const { name, email } = req.body;
-
     const usersRepository = getCustomRepository(UserRepository);
-
     const userExists = await usersRepository.findOne({ email });
 
     if(userExists) {
