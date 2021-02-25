db45b40025c10c7cbefe5013672ac5ed5c3748b7
MayronSouza <mayron.souza@gmz.solutions>
2021-02-25 07:54:58 -0300

    creating o SurveyController

---
+++ src/controllers/SurveyController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { SurveyRepository } from '../repositories/SurveyRepository';
+
+class SurveyController {
+  async create(req: Request, res: Response) {
+    const { title, description } = req.body;
+    const surveyRepository = getCustomRepository(SurveyRepository);
+    const survey = surveyRepository.create({ title, description });
+
+    await surveyRepository.save(survey);
+
+    return res.status(201).json(survey);
+  }
+}
+
+export { SurveyController };
